66b875d0694f6468f40852703e650f69d008cd32
Ruixin <tsuzsrx@gmail.com>
2026-01-15 15:03:15 -0400

    Format update 2025 data

--- 1-category-by-month.py
+++ 1-category-by-month.py
@@ -4,7 +4,7 @@ import shutil
 from concurrent.futures import ThreadPoolExecutor
 
 # Define the base directory where the files are stored
-base_dir = "/slow-array/ruixin/NOAA" 
+base_dir = "/meridian/AIS_archive/marinecadastre2"
 
 # Ensure the base directory exists
 if not os.path.exists(base_dir):
@@ -15,18 +15,22 @@ if not os.path.exists(base_dir):
 files = [f for f in os.listdir(base_dir) if f.endswith(".zip")]
 
 # Regular expressions for different file formats
-pattern_2015 = re.compile(r"(\d{4})_AIS_(\d{4})_(\d{2})_\d{2}\.zip")  # 2015-2024
+pattern_2025 = re.compile(r"(\d{4})_ais_(\d{4})-(\d{2})-\d{2}\.csv\.zst")  # 2025
+pattern_2015_2024 = re.compile(r"(\d{4})_AIS_(\d{4})_(\d{2})_\d{2}\.zip")  # 2015-2024
 pattern_2009_2014 = re.compile(r"(\d{4})_Zone\d+_(\d{4})_(\d{2})\.zip")  # 2009,2010,2014
 pattern_2011_2013 = re.compile(r"(\d{4})_Zone\d+_(\d{4})_(\d{2})\.gdb\.zip")  # 2011-2013
 
 # Function to process a file and move it to the corresponding folder
 def process_file(file):
-    match_2015 = pattern_2015.match(file)
+    match_2025 = pattern_2025.match(file)
+    match_2015_2024 = pattern_2015_2024.match(file)
     match_2009_2014 = pattern_2009_2014.match(file)
     match_2011_2013 = pattern_2011_2013.match(file)
 
-    if match_2015:
-        year, month = match_2015.group(2), match_2015.group(3)
+    if match_2025:
+        year, month = match_2025.group(1), match_2025.group(3)
+    elif match_2015_2024:
+        year, month = match_2015_2024.group(2), match_2015_2024.group(3)
     elif match_2009_2014:
         year, month = match_2009_2014.group(2), match_2009_2014.group(3)
     elif match_2011_2013:
